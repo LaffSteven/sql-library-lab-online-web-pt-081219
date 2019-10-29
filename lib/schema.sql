b500adc4d44d84663aa0b1009959b71bba016c06
Steven Laff <steve.laff@gmail.com>
2019-10-29 00:41:45 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -3,4 +3,4 @@ CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id 
 CREATE TABLE series(id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, sub-genre_id INTEGER);
 CREATE TABLE authors(id INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE sub-genres(id INTEGER PRIMARY KEY, name TEXT);
-INSERT INTO character_books (id, book_id, character_id)
+CREATE TABLE character_books (id INTEGER  PRIMARY KEY, book_id INTEGER, character_id INTEGER);
