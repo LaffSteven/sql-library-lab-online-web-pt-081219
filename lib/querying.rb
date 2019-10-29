6861e20e25e9798c1c6028b7d40c607d94419d41
Steven Laff <steve.laff@gmail.com>
2019-10-29 02:14:45 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -6,36 +6,36 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   " SELECT name, motto FROM characters 
-    ORDER BY (length(motto)) 
+    ORDER BY (LENGTH(motto)) 
     DESC LIMIT 1 "
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  " SELECT species, count(species) FROM characters 
+  " SELECT species, COUNT(species) FROM characters 
     GROUP BY species 
-    ORDER BY count(species) 
+    ORDER BY COUNT(species) 
     DESC LIMIT 1 "
 end
 
 def select_name_and_series_subgenres_of_authors
-  " SELECT authors.name, subgenres.name FROM authors 
-    INNER JOIN series ON authors.id = series.author_id 
-    INNER JOIN subgenres ON series.subgenre_id = subgenres.id "
+  " SELECT Authors.name, Subgenres.name FROM authors 
+    INNER JOIN series ON Authors.id = Series.author_id 
+    INNER JOIN subgenres ON Series.subgenre_id = Subgenres.id "
 end
 
 def select_series_title_with_most_human_characters
-  " SELECT series.title FROM series 
-    INNER JOIN authors ON authors.id = series.author_id 
-    INNER JOIN characters ON authors.id = characters.author_id WHERE characters.species = 'human' 
-    GROUP BY series.title 
-    ORDER BY count(characters.species) DESC
+  " SELECT Series.title FROM series 
+    INNER JOIN authors ON Authors.id = Series.author_id 
+    INNER JOIN characters ON Authors.id = Characters.author_id WHERE Characters.species = 'human' 
+    GROUP BY Series.title 
+    ORDER BY COUNT(Characters.species) DESC
     LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  " SELECT characters.name, count(character_books.id) FROM characters 
-    INNER JOIN character_books ON characters.id = character_books.character_id 
-    GROUP BY characters.name 
-    ORDER BY count(character_books.id) DESC"
+  " SELECT Characters.name, COUNT(Character_books.id) FROM characters 
+    INNER JOIN Character_books ON Characters.id = Character_books.Character_id 
+    GROUP BY Characters.name 
+    ORDER BY COUNT(Character_books.id) DESC"
 end
