24c0ca32ae836de1dfd0e383b951fe83c6a1a9bf
Steven Laff <steve.laff@gmail.com>
2019-10-29 01:13:20 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,7 +7,7 @@ INSERT INTO authors (id, name) VALUES (1, "J. R. R. Tolkien"), (2, "Frank Herber
 INSERT INTO books (id, title, year, series_id) VALUES (1, "The Hobbit", 1937, 1), (2, "The Fellowship of the Ring", 1954, 1), (3, "The Two Towers", 1954, 1), (4, "The Return of the King", 1955, 1),
 (5, "Dune", 1965, 2), (6, "Dune Messiah", 1969, 2), (7, "Children of Dune", 1976, 2);
 
-INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, "Frodo Baggins", "I carry the ring", "direwolf", 1), (2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1), (4, "Eddard Stark", "Winter is coming", "human", 1);
+INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, "Frodo Baggins", "We set out to save the Shire, Sam.", "Hobbit", 1), (2, "Samwise Gamgee", "A Lannister always pays his debts", "human", 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1), (4, "Eddard Stark", "Winter is coming", "human", 1);
 
 INSERT INTO characters (id, name, motto, species, author_id) VALUES (5, "Character One", "motto one", "cylon", 2), (6, "Character Two", "motto two", "human", 2), (7, "Character Three", "motto three", "cylon", 2), (8, "Character Four", "motto four", "cylon", 2);
 
