a2ac1dea737a037c079eab2209ccd6e1c31c38a2
Steven Laff <steve.laff@gmail.com>
2019-10-29 01:42:51 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,7 +9,7 @@ INSERT INTO books (id, title, year, series_id) VALUES (1, "The Hobbit", 1937, 1)
 
 INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, "Frodo Baggins", "We set out to save the Shire, Sam.", "Hobbit", 1), (2, "Samwise Gamgee", "Hold on Mr. Frodo.", "Hobbit", 1), (3, "Aragorn II Elessar", "There is still hope.", "Dúnedain", 1), (4, "Gandalf", "Fly, you fools.", "Maia", 1);
 
-INSERT INTO characters (id, name, motto, species, author_id) VALUES (5, "Paul Atreides", "I am your duke.", "Human", 2), (6, "Chani", "Tell me of the waters of your home.", "Fremen", 2), (7, "Vladimir Harkonen", " I'm alive! Ha-ha-ha!", "Human", 2), (8, "Thufir Hawat", "motto four", "cylon", 2);
+INSERT INTO characters (id, name, motto, species, author_id) VALUES (5, "Paul Atreides", "I am your duke.", "Human", 2), (6, "Chani", "Tell me of the waters of your home.", "Fremen", 2), (7, "Vladimir Harkonen", " I'm alive! Ha-ha-ha!", "Human", 2), (8, "Thufir Hawat", "Oh, my Duke, how I've failed you.", "Mentat", 2);
 
 INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
 
